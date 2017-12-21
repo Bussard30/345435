119c3c1f577248c557c44b0fe0ed1ee4521f57a9
daneil.sei <dnidel@edv230-15.wgg.local>
2017-12-21 14:23:34 +0100

    test Main edit daniel is toll

--- WebserverDatacollector/src/de/socket/server/Main.java
+++ WebserverDatacollector/src/de/socket/server/Main.java
@@ -9,7 +9,7 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+ 
 public class Main {
 	
 	private static ServerSocket serverSocket;
